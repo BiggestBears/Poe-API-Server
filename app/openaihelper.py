22d6ccab88e42392895c37a62bbb8eb016a0ddc5
BiggestBears <39621794+BiggestBears@users.noreply.github.com>
2024-04-20 17:55:38 +0800

    Add files via upload

--- app/openaihelper.py
+++ app/openaihelper.py
@@ -1,9 +1,9 @@
-import time, re, hashlib
+import time, re
 from config import config
 
 class OpenAIHelper:
     maxchecks = 240
-    message_hash_list = set()
+    lastprompt = ""
 
     def __init__(self, bot):
         self.bot = bot
@@ -54,84 +54,48 @@ class OpenAIHelper:
         
             formatted_msg = f"{role if not name else name}: {content}"
             formatted_messages.append(formatted_msg)
-        
-        if formatted_messages:
-            first_message_parts = formatted_messages[0].split("\n\n", 1)
-        if len(first_message_parts) > 1:
-            formatted_messages[0] = first_message_parts[1]
-            formatted_messages.append(first_message_parts[0])
-        return "  ".join(formatted_messages)
+        return "\n".join(formatted_messages)
+    
     def send_message(self, messages):
-        self.message_hash_list.add(self.latest_message_hash())
-        self.bot.clear_context()  
-        time.sleep(1)
         message = self.format_message(messages)
-
         if ("[ClaudeJB]" in message):
             message = message.replace("[ClaudeJB]", "")
-            old_message = self.bot.get_latest_message()
-            self.bot.send_message(message)
-            self.wating_newmessage()
-            for i in range(10):
-                message = self.bot.get_latest_message()
-                print("check message")
-                print(message)
-                if message != None and old_message!=message and len(message)>20:
-                    if  ":" in message or "*" in message:
-                        return
-                    else:
-                        break
+            if (self.lastprompt == message):
+                self.bot.delete_latest_message(True)
                 time.sleep(1)
-
-            self.bot.abort_message()
-            time.sleep(2)
-            self.bot.delete_latest_message()
-            self.message_hash_list.add(self.latest_message_hash())
-            self.bot.send_message(config.get("ClaudeJB", "I love it. Continue."))
+                self.bot.delete_latest_message(False)
+                self.bot.send_message(config.get("ClaudeJB", "I love it. Continue."))
+            else:
+                self.bot.clear_context()
+                self.lastprompt = message
+                self.bot.send_message(message, False)
+                self.bot.abort_message()
+                time.sleep(0.5)
+                self.bot.delete_latest_message()
+                time.sleep(1)
+                self.bot.send_message(config.get("ClaudeJB", "I love it. Continue."))
         else:
-            print(message)
+            self.bot.clear_context()
             self.bot.send_message(message)
-        
-
-    def latest_message_hash(self):
-        if (message := self.bot.get_latest_message()): 
-            return hashlib.md5(message.encode()).hexdigest()
-        else:
-            return ""
 
     def generate_completions(self, messages):
         self.send_message(messages)
         checks = 0
         while checks < self.maxchecks:
-            if not self.bot.is_generating() and self.bot.get_latest_message() != "" and self.latest_message_hash() not in self.message_hash_list:
+            if not self.bot.is_generating() and self.bot.get_latest_message() != "":
                 break
             checks += 1
             time.sleep(1)
         return self.generate_request(self.bot.get_latest_message(), "stop", "chat.completion")
     
-    def wating_newmessage(self):
-        for i in range(10):
-            if self.bot.get_latest_message() != "" and self.latest_message_hash() not in self.message_hash_list:
-                    break
-            time.sleep(0.5)
-            print("Wating newmessage...")
-
-
     def generate_completions_stream(self):
         checks = 0
         old_message_length = 0
-        new_message_length = 0
-        message = ""
-        new_message =""
-        print("generate_completions_stream start")
         while checks < self.maxchecks:
-            if not self.bot.is_generating() and self.bot.get_latest_message() != "" and self.latest_message_hash() not in self.message_hash_list:
-                print("generate_completions_stream break " + str(new_message_length) + " " + str(old_message_length))
+            if not self.bot.is_generating() and self.bot.get_latest_message() != "":
                 break
             time.sleep(1)
             message = self.bot.get_latest_message()
-            if (message == None or self.latest_message_hash() in self.message_hash_list):
-                continue
             message = message.rstrip('\n')
             new_message_length = len(message)
             new_message = message[old_message_length:new_message_length]
@@ -140,18 +104,7 @@ class OpenAIHelper:
             
             if new_message != "":
                 yield self.generate_request(new_message, None, "chat.completion.chunk")
-        
-        lastmsg = self.bot.get_latest_message()
-       
-        
-        final_message = ""
-        if lastmsg != None and lastmsg != "":
-            print("lastmsg:" + str(lastmsg))
-            final_message = lastmsg[old_message_length:]
+        final_message = self.bot.get_latest_message()[old_message_length:]
         if final_message != "":
-            print("generate_completions_stream final_message:"  + final_message)
-            
             yield self.generate_request(final_message, "stop", "chat.completion.chunk")
         yield self.generate_request("", "stop", "chat.completion.chunk")
-
-        print("generate_completions_stream end")
--- app/poebot.py
+++ app/poebot.py
@@ -6,10 +6,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from functools import wraps
 from selenium.common.exceptions import WebDriverException, TimeoutException
-import markdownify, time, secrets, string, os, glob
+import markdownify, time, secrets, string, os, glob, hashlib
 from config import config
 import undetected_chromedriver as uc
-from selenium.webdriver.common.proxy import Proxy, ProxyType
 
 def handle_errors(func):
     @wraps(func)
@@ -18,62 +17,38 @@ def handle_errors(func):
             return func(self, *args, **kwargs)
         except WebDriverException as e:
             print(f"An error occurred: {e}")
-            self.kill_driver()
+            url = self.driver.current_url
             time.sleep(3)
-            self.start_driver()
+            self.kill_driver()
+            time.sleep(1)
+            self.start_driver(url)
     return wrapped_func
 
 class PoeBot:
+    message_hash_list = set()
     def __init__(self):
         self.start_driver()
-        
-    def start_driver(self):
-        isReady = False
-        for i in range(30):
-            try:
-                self.start_driver_II()
-                isReady=True
-                break
-            except Exception as e:
-                print("error", e)
-                self.kill_driver()
-                time.sleep(3)
-                print("retry...")
-                
-                pass
-        if isReady==False:
-            self.start_driver_II()
 
-    def start_driver_II(self):
+    def start_driver(self, url = None):
         if (config["cookie"] is None or config["bot"] is None):
             return
-        
-        PROXY = config.get("proxy","")
         options = webdriver.ChromeOptions()
-
-        if PROXY!=None:
-            options.add_argument('--proxy-server=' + PROXY)
-            #options.add_argument("--ignore-certificate-errors")
-            print("user proxy:" + '--proxy-server=' + PROXY)
-
-        print("View:" + str(config.get("view","0")) + " " + str(config.get("view","0") !="1"))
-        #options.add_argument("ignore-certificate-errors")
-        
-        self.driver = uc.Chrome(options=options, headless= config.get("view","0") !="1" )
-        
+        self.driver = uc.Chrome(options=options, headless=False)
         self.driver.get("https://poe.com/login?redirect_url=%2F")
         self.driver.add_cookie({"name": "p-b", "value": config['cookie']})
-        self.driver.get(f"https://poe.com/{config['bot']}")
+        if (url):
+            self.driver.get(url)
+        else:
+            self.driver.get(f"https://poe.com/{config['bot']}")
         
     
     @handle_errors
     def get_latest_message(self):
-        bot_messages = self.driver.find_elements(By.XPATH, '//div[contains(@class, "Message_botMessageBubble__CPGMI")]')
+        bot_messages = self.driver.find_elements(By.XPATH, '//div[contains(@class, "Message_botMessageBubble__")]')
         if bot_messages:
             latest_message = bot_messages[-1]
             if (latest_message.text == "..."):
                 return None
-            self.driver.execute_script('arguments[0].scrollIntoView();',latest_message)
             msg = markdownify.markdownify(latest_message.get_attribute("innerHTML"), heading_style="ATX")
             msg = msg.replace("\*", "*")
             return msg
@@ -83,40 +58,31 @@ class PoeBot:
     @handle_errors
     def abort_message(self):
         try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "ChatStopMessageButton_stopButton__LWNj6"))).click()
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[contains(@class, 'ChatStopMessageButton_stopButton__')]"))).click()
         except TimeoutException:
             return
 
     @handle_errors
-    def send_message(self, message):
-        if ("[TEXT]" in message):
-            message = message.replace("[TEXT]", "")
-            self.send_message_as_text(message)
+    def send_message(self, message, wait_for_message = True):
+        self.message_hash_list.add(self.latest_message_hash())
+        if (len(message) > config.get("send-as-text-limit", 200)):
+            self.send_message_as_file(message)
         else:
-            if (len(message) > config.get("send-as-text-limit", 200)):
-                self.send_message_as_file(message)
-            else:
-                self.send_message_as_text(message)
+            self.send_message_as_text(message)
         time.sleep(1)
+        if (config.get("autorefresh", True) == True):
+            self.driver.refresh()
+            time.sleep(1)
+            self.driver.execute_script("""var xpath = '//*[@id="__next"]/div[1]/div/main/div/div';var scrollElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;scrollElement.scrollTop = scrollElement.scrollHeight;""")
         start_time = time.time()
-        latest_message = ""
-        latest_message_old = ""
-        while True:
-            bot_messages = self.driver.find_elements(By.XPATH, '//div[contains(@class, "Message_botMessageBubble__CPGMI")]')
-            if bot_messages:
-                latest_message_old = latest_message
-                latest_message = bot_messages[-1].text
-            if latest_message != "..." and latest_message_old!=latest_message:
-                print("break:" + latest_message + " " +  latest_message_old)
-                break
-            if self.is_generating() == False and time.time() - start_time > 30:
-                print("Wating Timeout waiting for bot message...")
-                break
-            if (self.driver.find_elements(By.XPATH, "//div[@data-visible='true' and contains(@class, 'Message_humanOptimisticFooter__zm1hu') and text()='Message failed to send.']")):
-                self.reload()
+        while wait_for_message:
+            latest_message = self.get_latest_message()
+            if latest_message and not self.latest_message_in_hashlist():
+                return
             if time.time() - start_time > 120:
-                self.kill_driver()
-                raise Exception("Timeout waiting for bot message")
+                self.driver.refresh()
+                print("Timeout waiting for bot message")
+                return
             time.sleep(1)
 
     @handle_errors
@@ -131,38 +97,35 @@ class PoeBot:
         open(txt_file_path, 'w', encoding='utf-8').write(message)
         absolute_path = os.path.abspath(txt_file_path)
 
-        file_input = self.driver.find_element(By.CLASS_NAME, 'ChatMessageFileInputButton_input__szx6_')
+        file_input = self.driver.find_element(By.XPATH, "//*[contains(@class, 'ChatMessageFileInputButton_input__')]")
         file_input.send_keys(absolute_path)
         
-        text_area = self.driver.find_element(By.CLASS_NAME, "GrowingTextArea_textArea__eadlu")
+        text_area = self.driver.find_element(By.XPATH, "//textarea[contains(@class, 'GrowingTextArea_textArea__')]")
         text_area.send_keys(config.get("instruction", "-"))
         text_area.send_keys(Keys.RETURN)
     
     @handle_errors
     def send_message_as_text(self, message):
-        text_area = self.driver.find_element(By.CLASS_NAME, "GrowingTextArea_textArea__eadlu")
-        for part in message.split('\n'):
-            text_area.send_keys(part)
-            text_area.send_keys(Keys.SHIFT,Keys.ENTER)
-           
-        #message = message.replace("\n", " ")
-        #text_area.send_keys(message)
+        text_area = self.driver.find_element(By.XPATH, "//textarea[contains(@class, 'GrowingTextArea_textArea__')]")
+        message = message.replace("\n", " ")
+        text_area.send_keys(message)
         text_area.send_keys(Keys.RETURN)
 
 
     @handle_errors
     def clear_context(self):
-        clear_button = self.driver.find_element(By.CLASS_NAME, "ChatBreakButton_button__EihE0")
+        clear_button = self.driver.find_element(By.XPATH, "//*[contains(@class, 'ChatBreakButton_button__')]")
         clear_button.click()
+        time.sleep(1)
 
     @handle_errors
     def is_generating(self):
-        stop_button_elements = self.driver.find_elements(By.CLASS_NAME, "ChatStopMessageButton_stopButton__LWNj6")
+        stop_button_elements = self.driver.find_elements(By.XPATH, "//*[contains(@class, 'ChatStopMessageButton_stopButton__')]")
         return len(stop_button_elements) > 0
     
     @handle_errors
     def get_suggestions(self):
-        suggestions_container = self.driver.find_elements(By.CLASS_NAME, "ChatMessageSuggestedReplies_suggestedRepliesContainer__JgW12")
+        suggestions_container = self.driver.find_elements(By.XPATH, "//*[contains(@class, 'ChatMessageSuggestedReplies_suggestedRepliesContainer__')]")
         if not suggestions_container:
             return []
         suggestion_buttons = suggestions_container[0].find_elements(By.TAG_NAME, "button")
@@ -171,38 +134,40 @@ class PoeBot:
     @handle_errors
     def delete_latest_message(self, bot = True):
         if (bot):
-            messages = self.driver.find_elements(By.XPATH, '//div[contains(@class, "Message_botMessageBubble__CPGMI")]')
+            messages = self.driver.find_elements(By.XPATH, '//div[contains(@class, "Message_botMessageBubble__")]')
         else:
-            messages = self.driver.find_elements(By.XPATH, '//div[contains(@class, "Message_humanMessageBubble__Nld4j")]')
+            messages = self.driver.find_elements(By.XPATH, '//div[contains(@class, "Message_humanMessageBubble__")]')
         if (len(messages) == 0):
             return
         latest_message = messages[-1]
-        self.driver.execute_script('arguments[0].scrollIntoView();',latest_message)
-        time.sleep(2)
         ActionChains(self.driver).context_click(latest_message).perform()
-        delete_button = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(((By.XPATH, "//button[starts-with(@class, 'DropdownMenuItem_item__nYv_0') and contains(., 'Delete...')]"))))
+
+        delete_button = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(((By.XPATH, "//button[starts-with(@class, 'DropdownMenuItem_item__') and contains(text(), 'Delete...')]"))))
         ActionChains(self.driver).move_to_element(delete_button).click().perform()
-        confirm1_button = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".Button_buttonBase__0QP_m.Button_danger__zI3OH")))
+
+        confirm1_button = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(((By.XPATH, "//*[contains(@class, 'ChatPageDeleteFooter_button__')]"))))
         ActionChains(self.driver).move_to_element(confirm1_button).click().perform()
-        confirm2_button = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(((By.XPATH, "//button[@class='Button_buttonBase__0QP_m Button_danger__zI3OH']"))))
+
+        confirm2_button = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(((By.XPATH, "//*[contains(@class, 'Button_danger__') and not(contains(@class, 'ChatPageDeleteFooter_button__'))]"))))
         ActionChains(self.driver).move_to_element(confirm2_button).click().perform()
-
-  
     
-    @handle_errors
-    def reload(self):
-        self.driver.refresh()
-
     def kill_driver(self):
         if hasattr(self, "driver"):
             self.driver.quit()
-
-    def get_driver(self):
-        if hasattr(self, "driver"):
-            return self.driver
 
     def __del__(self):
         if hasattr(self, "driver"):
             self.kill_driver()
 
-    
+    def add_message_hash(self, hash):
+        if hash:
+            self.message_hash_list.add(hash)
+
+    def latest_message_hash(self):
+        message = self.get_latest_message()
+        return hashlib.md5(message.encode()).hexdigest() if message else None
+        
+    def latest_message_in_hashlist(self):
+        hash = self.latest_message_hash()
+        if hash:
+            return hash in self.message_hash_list
